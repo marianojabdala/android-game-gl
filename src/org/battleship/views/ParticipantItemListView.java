1eff26db77c3d69ea6b106af31aafd768d7b5f8b
zetahernandez <zetahernandez@localhost>
2011-10-19 21:29:09 -0300

    User icon in participants list

--- src/org/battleship/views/ParticipantItemListView.java
+++ src/org/battleship/views/ParticipantItemListView.java
@@ -1,10 +1,12 @@
 package org.battleship.views;
 
+import org.battleship.R;
 import org.battleship.model.Participant;
 
 import android.content.Context;
 import android.graphics.Color;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,7 +16,8 @@ import android.widget.TextView;
  */
 public class ParticipantItemListView extends LinearLayout {
 	private TextView userNameTV;
-
+	private ImageView userIcon;
+	
 	public ParticipantItemListView(Context context, Participant participant) {
 
 		super(context);
@@ -26,10 +29,15 @@ public class ParticipantItemListView extends LinearLayout {
 		params.setMargins(5, 3, 5, 0);
 		params.height = 50;
 
+		this.userIcon = new ImageView(context);
+    	this.userIcon.setImageDrawable(getResources().getDrawable(R.drawable.user));
+		this.addView(userIcon);
+		
 		this.userNameTV = new TextView(context);
 		this.userNameTV.setText(participant.username);
 		this.userNameTV.setTextSize(16f);
 		this.userNameTV.setTextColor(Color.GREEN);
 		this.addView(this.userNameTV, params);
+		
 	}
 }
